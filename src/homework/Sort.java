48d618f6b132c5b54e646904d43b4079666b8663
Grisha Grigoryan <grisha_grigoryan_0@mail.ru>
2025-10-31 23:50:55 +0400

    Add homework sort

---
+++ src/homework/Sort.java
@@ -0,0 +1,21 @@
+package homework;
+
+
+public class Sort {
+    static void main() {
+        int[] array = {4, 7, 1, 3, 9, 0, 2};
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        for (int x : array) {
+            System.out.print(x + " ");
+        }
+    }
+}
